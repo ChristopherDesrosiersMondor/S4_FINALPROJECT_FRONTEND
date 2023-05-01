1bd48f419d3700e92f9487baa92ca9d30a1b9e5a
valerieouellette <valerieouellette@icloud.com>
2023-05-01 12:06:04 -0400

    routes

--- interface_web/src/App.js
+++ interface_web/src/App.js
@@ -3,7 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <Fragment>
+        <BrowserRouter>
+            <Menu className='menu'>
+              <MenuItem className='menuItem' as={NavLink} to='/' exact='true'>Home</MenuItem>
+              <MenuItem className='menuItem' as={NavLink} to='/popular'>Popular</MenuItem>
+            </Menu>
+
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path='/popular' element={<Popular/>}/>
+            <Route path='*' element={<Page404/>}/>
+          </Routes>
+
+        </BrowserRouter>
+      </Fragment>
+
     </div>
   );
 }
