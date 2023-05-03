af2b70f052e8b79bd485ba40aa20abea73ab922c
valerieouellette <valerieouellette@icloud.com>
2023-05-03 15:38:58 -0400

    connexion form

--- interface_mobile/interface_mobile/lib/connexion.dart
+++ interface_mobile/interface_mobile/lib/connexion.dart
@@ -7,9 +7,70 @@ class ConnexionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Connexion Page'),
+        title: const Text('ConnexionPage'),
       ),
-      body: const Center(child: Text('Page de connexion')),
+      body: const Center(child: ConnexionForm()),
+    );
+  }
+}
+
+class ConnexionForm extends StatefulWidget {
+  const ConnexionForm({super.key});
+
+  @override
+  State<ConnexionForm> createState() => ConnexionFormState();
+}
+
+class ConnexionFormState extends State<ConnexionForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Pseudo',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: 'Mot de passe',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState!.validate()) {
+                  // Process data.
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
