535c2f2a3a913b5d86f85285ebe4c29b0425e780
ChristopherDesrosiersMondor <desrosch@gmail.com>
2023-05-04 14:24:52 -0400

    svelteblog tests

---
+++ test_svelte/src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'Documentation'
+export const description = 'Hublot blog for documenting our process'
+export const url = dev ? 'http://localhost:5173/' : 'https://joyofcode.xyz/'
