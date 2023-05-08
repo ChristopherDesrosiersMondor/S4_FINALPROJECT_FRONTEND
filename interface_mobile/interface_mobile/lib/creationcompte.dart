6db6c699b7652a790f26c8d3bc37c69631b2df70
valerieouellette <valerieouellette@icloud.com>
2023-05-08 13:52:46 -0400

    Update creationcompte.dart

--- interface_mobile/interface_mobile/lib/creationcompte.dart
+++ interface_mobile/interface_mobile/lib/creationcompte.dart
@@ -118,14 +118,8 @@ class CreationCompteFormState extends State<CreationCompteForm> {
                   lastDate: DateTime(2101));
 
               if (pickedDate != null) {
-                print(
-                    pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
                 String formattedDate =
                     DateFormat('yyyy-MM-dd').format(pickedDate);
-                print(
-                    formattedDate); //formatted date output using intl package =>  2021-03-16
-                //you can implement different kind of Date Format here according to your requirement
-
                 setState(() {
                   birthdayController.text =
                       formattedDate; //set output date to TextField value.
