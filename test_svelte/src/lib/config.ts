347c8d898904dddf02b0078a7cf7d37044167b19
ChristopherDesrosiersMondor <desrosch@gmail.com>
2023-05-08 09:45:06 -0400

    deleting test, beggining real one in svelte

--- test_svelte/src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from '$app/environment'
-
-export const title = 'Documentation'
-export const description = 'Hublot blog for documenting our process'
-export const url = dev ? 'http://localhost:5173/' : 'https://joyofcode.xyz/'
