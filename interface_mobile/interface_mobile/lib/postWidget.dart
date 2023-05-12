0b8ff8de836ccdb6ca2c021f0283116d540ae2ec
valerieouellette <valerieouellette@icloud.com>
2023-05-12 10:12:37 -0400

    start post widget

---
+++ interface_mobile/interface_mobile/lib/postWidget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PostWidget extends StatelessWidget {
+  const PostWidget({super.key, required this.username, required this.password});
+
+  final String username;
+  final String password;
+
+  @override
+  Widget build(BuildContext context) {}
+}
