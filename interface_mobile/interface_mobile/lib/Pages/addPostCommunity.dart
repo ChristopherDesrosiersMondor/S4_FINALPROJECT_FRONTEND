a45e6528ea9ce04c8e417364ac250bc70d4a48c9
valerieouellette <valerieouellette@icloud.com>
2023-05-17 11:46:04 -0400

    Update addPostCommunity.dart

--- interface_mobile/interface_mobile/lib/Pages/addPostCommunity.dart
+++ interface_mobile/interface_mobile/lib/Pages/addPostCommunity.dart
@@ -37,61 +37,61 @@ class AddCommunityToPostForm extends StatefulWidget {
 class AddCommunityToPostFormState extends State<AddCommunityToPostForm> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Http Get Request."),
-          leading: Icon(
-            Icons.get_app,
-          ),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(16.0),
-          child: FutureBuilder(
-            future: getAllCommunities(),
-            builder: (BuildContext ctx, AsyncSnapshot snapshot) {
-              if (snapshot.data == null) {
-                return Container(
-                  child: Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                );
-              } else {
-                return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (ctx, index) => ListTile(
-                    title: Text(snapshot.data[index].communityName),
-                    subtitle: Text(snapshot.data[index].id),
-                    contentPadding: EdgeInsets.only(bottom: 20.0),
-                  ),
-                );
-              }
-            },
-          ),
-        ),
-      ),
-    );
-    // return Container(
-    //   padding: const EdgeInsets.all(10),
-    //   child: FutureBuilder(
-    //     future: getAllCommunities(),
-    //     builder: (BuildContext ctx, AsyncSnapshot snapshot) {
-    //       if (snapshot.data == null) {
-    //         return const Center(
-    //           child: CircularProgressIndicator(),
-    //         );
-    //       } else {
-    //         return ListView.builder(
-    //           itemCount: snapshot.data.length,
-    //           itemBuilder: (ctx, index) => CommunityWidget(
-    //             communityId: snapshot.data[index].id,
-    //             communityName: snapshot.data[index].communityName,
-    //           ),
-    //         );
-    //       }
-    //     },
+    // return SafeArea(
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text("Http Get Request."),
+    //       leading: Icon(
+    //         Icons.get_app,
+    //       ),
+    //     ),
+    //     body: Container(
+    //       padding: EdgeInsets.all(16.0),
+    //       child: FutureBuilder(
+    //         future: getAllCommunities(),
+    //         builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+    //           if (snapshot.data == null) {
+    //             return Container(
+    //               child: Center(
+    //                 child: CircularProgressIndicator(),
+    //               ),
+    //             );
+    //           } else {
+    //             return ListView.builder(
+    //               itemCount: snapshot.data.length,
+    //               itemBuilder: (ctx, index) => ListTile(
+    //                 title: Text(snapshot.data[index].communityName),
+    //                 subtitle: Text(snapshot.data[index].id),
+    //                 contentPadding: EdgeInsets.only(bottom: 20.0),
+    //               ),
+    //             );
+    //           }
+    //         },
+    //       ),
+    //     ),
     //   ),
     // );
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: FutureBuilder(
+        future: getAllCommunities(),
+        builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (ctx, index) => CommunityWidget(
+                communityId: snapshot.data[index].id,
+                communityName: snapshot.data[index].communityName,
+              ),
+            );
+          }
+        },
+      ),
+    );
     // return Configuration.communityChoices("communityName");
   }
 }
