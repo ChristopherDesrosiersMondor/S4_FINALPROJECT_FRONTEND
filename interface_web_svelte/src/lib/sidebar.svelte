1b267ec130994ed346eacfd3431088c7bee7c06d
ChristopherDesrosiersMondor <desrosch@gmail.com>
2023-05-29 09:51:31 -0400

    Update sidebar.svelte

--- interface_web_svelte/src/lib/sidebar.svelte
+++ interface_web_svelte/src/lib/sidebar.svelte
@@ -2,69 +2,65 @@
 	import AccountCreation from '$lib/create_account.svelte';
 	import { Currency, Gamepad2, Home, LineChart, Star, TrendingUp, Trophy, Tv } from 'lucide-svelte';
 	import '../app.css';
-	import { isCommunityPage, isVisible } from '../stores';
+	import { isVisible } from '../stores';
 </script>
 
-{#if $isCommunityPage}
-	<div />
-{:else}
-	<div class="sidebar-container">
-		<div role="menu" class="sidebar-menu-container">
-			<div class="sidebar-menu-section-title">Feeds</div>
-			<a href="/" role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./">
-				<i class="sidebar-icons icon"><Home /></i>
-				<span class="sidebar-items-text">Home</span>
-			</a>
-			<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
-				<i class="sidebar-icons icon"><TrendingUp /></i>
-				<span class="sidebar-items-text">Popular</span>
-			</a>
-			<div class="sidebar-menu-section-title">Recent</div>
-			<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
-				<i class="sidebar-icons icon"><Trophy /></i>
-				<span class="sidebar-items-text">h/NoStupidQuestion</span>
-			</a>
-			<div class="sidebar-menu-section-title">Topics</div>
-			<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
-				<i class="sidebar-icons icon"><Gamepad2 /></i>
-				<span class="sidebar-items-text">Gaming</span>
-			</a>
-			<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
-				<i class="sidebar-icons icon"><Trophy /></i>
-				<span class="sidebar-items-text">Sports</span>
-			</a>
-			<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
-				<i class="sidebar-icons icon"><LineChart /></i>
-				<span class="sidebar-items-text">Business, Economics, and finance</span>
-			</a>
-			<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
-				<i class="sidebar-icons icon"><Currency /></i>
-				<span class="sidebar-items-text">Crypto</span>
-			</a>
-			<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
-				<i class="sidebar-icons icon"><Tv /></i>
-				<span class="sidebar-items-text">Television</span>
-			</a>
-			<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
-				<i class="sidebar-icons icon"><Star /></i>
-				<span class="sidebar-items-text">Celebrity</span>
-			</a>
-		</div>
-
-		{#if $isVisible}
-			<div class="sidebar-footer-container">
-				<div class="simple-divider-line-container" />
-				<div class="l">
-					<p>
-						Create an account to follow your favorite communities and participate in conversations.
-					</p>
-					<br />
-					<AccountCreation />
-				</div>
-			</div>
-		{/if}
+<div class="sidebar-container">
+	<div role="menu" class="sidebar-menu-container">
+		<div class="sidebar-menu-section-title">Feeds</div>
+		<a href="/" role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./">
+			<i class="sidebar-icons icon"><Home /></i>
+			<span class="sidebar-items-text">Home</span>
+		</a>
+		<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
+			<i class="sidebar-icons icon"><TrendingUp /></i>
+			<span class="sidebar-items-text">Popular</span>
+		</a>
+		<div class="sidebar-menu-section-title">Recent</div>
+		<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
+			<i class="sidebar-icons icon"><Trophy /></i>
+			<span class="sidebar-items-text">h/NoStupidQuestion</span>
+		</a>
+		<div class="sidebar-menu-section-title">Topics</div>
+		<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
+			<i class="sidebar-icons icon"><Gamepad2 /></i>
+			<span class="sidebar-items-text">Gaming</span>
+		</a>
+		<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
+			<i class="sidebar-icons icon"><Trophy /></i>
+			<span class="sidebar-items-text">Sports</span>
+		</a>
+		<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
+			<i class="sidebar-icons icon"><LineChart /></i>
+			<span class="sidebar-items-text">Business, Economics, and finance</span>
+		</a>
+		<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
+			<i class="sidebar-icons icon"><Currency /></i>
+			<span class="sidebar-items-text">Crypto</span>
+		</a>
+		<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
+			<i class="sidebar-icons icon"><Tv /></i>
+			<span class="sidebar-items-text">Television</span>
+		</a>
+		<a role="menuitem" class="sidebar-menu-link" tabindex="-1" aria-label="./" href="./">
+			<i class="sidebar-icons icon"><Star /></i>
+			<span class="sidebar-items-text">Celebrity</span>
+		</a>
 	</div>
-{/if}
+
+	{#if $isVisible}
+		<div class="sidebar-footer-container">
+			<div class="simple-divider-line-container" />
+			<div class="l">
+				<p>
+					Create an account to follow your favorite communities and participate in conversations.
+				</p>
+				<br />
+				<AccountCreation />
+			</div>
+		</div>
+	{/if}
+</div>
 
 <!-- Source: https://www.w3schools.com/howto/howto_css_fixed_sidebar.asp -->
 
