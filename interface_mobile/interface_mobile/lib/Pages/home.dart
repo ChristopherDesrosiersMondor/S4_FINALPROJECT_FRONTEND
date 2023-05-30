7d797b2dc6aa0dba8f430da04aefe379728de7c4
valerieo <valerieouellette@icloud.com>
2023-05-29 21:53:32 -0400

    basic affichage username

--- interface_mobile/interface_mobile/lib/Pages/home.dart
+++ interface_mobile/interface_mobile/lib/Pages/home.dart
@@ -9,6 +9,10 @@ import 'package:provider/provider.dart';
 import '../Entities/post.dart';
 import '../config.dart';
 
+// Sources
+// How to put a future builer inside a colum
+//https://stackoverflow.com/questions/52335903/flutter-put-futurebuilder-widget-inside-a-column
+
 const List<String> list = <String>['Home', 'Popular', 'News', 'New on Hublot!'];
 
 class Home extends StatefulWidget {
@@ -60,12 +64,6 @@ class HomeState extends State<Home> {
               ),
               actions: <Widget>[
                 IconButton(icon: const Icon(Icons.search), onPressed: () {}),
-                (value.userId != null)
-                    ? Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: Text("Hi" + value.username))
-                    : const Padding(
-                        padding: EdgeInsets.all(5), child: Text("Hi!")),
                 IconButton(
                   icon: const Icon(Icons.account_circle),
                   onPressed: () {
@@ -82,32 +80,61 @@ class HomeState extends State<Home> {
               backgroundColor: Configuration.appDarkBackgroundColor,
             ),
             backgroundColor: Configuration.appDarkBackgroundColor,
-            body: FutureBuilder(
-              future: getAllPost(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  List<Post> posts = snapshot.data;
-                  return ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      itemCount: posts.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return PostWidget(
-                            refreshPage: refresh,
-                            postId: posts[index].id,
-                            postTitle: posts[index].postTitle,
-                            postContent: posts[index].postContent,
-                            postSource: posts[index].postSource,
-                            postDate: posts[index].postDate.toString(),
-                            postUpVote: posts[index].postUpVote,
-                            postDownVote: posts[index].postDownVote,
-                            postUserId: posts[index].postIdUser,
-                            postCommId: posts[index].postIdCom,
-                            username: posts[index].username,
-                            communityName: posts[index].postIdCom.toString());
-                      });
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
+            body: Builder(
+              builder: (context) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 0,
+                      child: (value.userId != null)
+                          ? Padding(
+                              padding: const EdgeInsets.all(5),
+                              child: Text(
+                                "Welcome " + value.username,
+                                style: Configuration.mainContentTextForApp(
+                                    Colors.white, 20),
+                              ))
+                          : const Padding(
+                              padding: EdgeInsets.all(5), child: Text("")),
+                    ),
+                    Expanded(
+                        flex: 2,
+                        child: FutureBuilder(
+                          future: getAllPost(),
+                          builder:
+                              (BuildContext context, AsyncSnapshot snapshot) {
+                            if (snapshot.hasData) {
+                              List<Post> posts = snapshot.data;
+                              return ListView.builder(
+                                  scrollDirection: Axis.vertical,
+                                  itemCount: posts.length,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    return PostWidget(
+                                        refreshPage: refresh,
+                                        postId: posts[index].id,
+                                        postTitle: posts[index].postTitle,
+                                        postContent: posts[index].postContent,
+                                        postSource: posts[index].postSource,
+                                        postDate:
+                                            posts[index].postDate.toString(),
+                                        postUpVote: posts[index].postUpVote,
+                                        postDownVote: posts[index].postDownVote,
+                                        postUserId: posts[index].postIdUser,
+                                        postCommId: posts[index].postIdCom,
+                                        username: posts[index].username,
+                                        communityName:
+                                            posts[index].postIdCom.toString());
+                                  });
+                            } else {
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            }
+                          },
+                        ))
+                  ],
+                );
               },
             )));
   }
